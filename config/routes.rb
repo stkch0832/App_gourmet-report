f926b9ddfeea0b596ac877e3a4eb69e59a94b80a
stkch0832 <stkch0832@gmail.com>
2021-10-15 20:54:49 +0900

    i18n install

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  
+
   
   get 'posts/index'
+  get "posts/:id" => "posts#show"
+  
   get '/' => 'home#index'
   get 'home/about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
