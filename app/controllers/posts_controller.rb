eb6aaf6941845f7b8d6be4108343bf3510efa51e
stkch0832 <stkch0832@gmail.com>
2021-10-26 22:22:43 +0900

    form 作成(編集・削除)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,47 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    # .order(create_at: desc)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      shop_name: params[:shop_name],
+      shop_category: params[:shop_category],
+      area_name: params[:area_name],
+      product_category: params[product_category],
+      comment: params[:comment],
+      )
+    @post.save
+    redirect_to("/posts/index")
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.shop_name = params[:shop_name]
+    @post.shop_category = params[:shop_category]
+    @post.area_name =  params[:area_name]
+    @post.product_category =  params[:product_category]
+    @post.comment =  params[:comment]
+    @post.save
+    redirect_to("/posts/index")
+  end
+  
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
   
-  get 'posts/index'
+  get 'posts/index' => 'posts#index'
+  get 'posts/new' => 'posts#new'
   get "posts/:id" => "posts#show"
+  post "posts/create" => "posts#create"
   
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post 'posts/:id/destroy' => 'posts#destroy'
+
   get '/' => 'home#index'
   get 'home/about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
