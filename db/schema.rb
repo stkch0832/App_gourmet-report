d80d18b85f7e8f6711a588de17ff1631f232dd8c
stkch0832 <stkch0832@gmail.com>
2021-11-09 22:00:23 +0900

    has_secure_password 追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_114555) do
+ActiveRecord::Schema.define(version: 2021_11_09_120425) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_114555) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
